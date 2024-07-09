623f361f7a4029e541445faaa822f39eecb1feb7
essence- <6ejlo3epobnt@gmail.com>
2024-07-09 23:14:04 +0300

    fear(homepage): add content to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
 import styles from "./styles/mainpage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div> home page </div>;
+  return (
+    <div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.mainText}> Take care of your plants</h1>
+        <p className={styles.subText}>
+          {" "}
+          and we'll help you with it <br />
+        </p>
+      </div>
+      <div className={styles.textContainer}>
+        <p className={styles.subText}>
+          {" "}
+          Lets know about{" "}
+          <Link href="/about" passHref>
+            {" "}
+            us
+          </Link>{" "}
+          a little bit more
+        </p>
+      </div>
+    </div>
+  );
 }
