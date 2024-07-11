0e5d828661aa4ab7cb4f1010a5300091013375e9
plaffyyy <k.zimin@innopolis.university>
2024-07-12 00:43:45 +0300

    profile page is done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import emailIcon from "../../../public/assets/emailSign.png";
 import locationIcon from "../../../public/assets/locationSign.png";
 import profilePhoto from "../../../public/assets/person.jpeg";
+import romashiPhoto from "../../../public/assets/romashki.png";
 
 const profilePage = () => {
   return (
@@ -50,9 +51,42 @@ const profilePage = () => {
         <div className={styles.calendarContainer}>
         <h1 className={styles.calendarHead}>Calendar preview</h1>
             <div className={styles.calendarPreview}>
-              
+              <div className={styles.Day}>
+                <div className={styles.dayLeftPart}>
+                  <p className={styles.textDay}>Mon</p>
+                  <p className={styles.circle}> 8 </p>
+                </div>
+                <div className={styles.dayRightPart}>
+                  <div className={styles.plantOne}>
+                    <p className={styles.plantName}>Rose</p>
+                  </div>
+                  <div className={styles.plantOne}>
+                    <p className={styles.plantName}>Cactus</p>
+                  </div>
+                  <div className={styles.plantOne}>
+                    <p className={styles.plantName}>Tomato</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.Day}>
+                <div className={styles.dayLeftPart}>
+                  <p className={styles.textDay}>Mon</p>
+                  <p className={styles.circle}> 8 </p>
+                </div>
+                <div className={styles.dayRightPart}>
+                  <div className={styles.plantOne}>
+                    <p className={styles.plantName}>Rose</p>
+                  </div>
+                  <div className={styles.plantOne}>
+                    <p className={styles.plantName}>Cactus</p>
+                  </div>
+                  <div className={styles.plantOne}>
+                    <p className={styles.plantName}>Tomato</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div>
+            <div className={styles.toCalendarButtonContainer}>
               <Link href="/calendar" passHref>
                 <button className={styles.toCalendarButton}>Go to calendar</button>
               </Link>
@@ -60,7 +94,117 @@ const profilePage = () => {
         </div>
       </div>
       <div className={styles.containerCollection}>
-
+          <h1 className={styles.collectionPreview}> Plant collection</h1>
+          <div className={styles.collection}>
+              <div className={styles.plant}>
+                <div className={styles.plantInfo}>
+                  <Image
+                    className={styles.plantImage}
+                    src={romashiPhoto}
+                    alt={"personInitPage"}
+                    width={166}
+                    height={134}
+                  />
+                  <p className={styles.plantName}> Romashka </p>
+                </div>
+              </div>
+              <div className={styles.plant}>
+                <div className={styles.plantInfo}>
+                  <Image
+                    className={styles.plantImage}
+                    src={romashiPhoto}
+                    alt={"personInitPage"}
+                    width={166}
+                    height={134}
+                  />
+                  <p className={styles.plantName}> Romashka </p>
+                </div>
+              </div>
+              <div className={styles.plant}>
+                <div className={styles.plantInfo}>
+                  <Image
+                    className={styles.plantImage}
+                    src={romashiPhoto}
+                    alt={"personInitPage"}
+                    width={166}
+                    height={134}
+                  />
+                  <p className={styles.plantName}> Romashka </p>
+                </div>
+              </div>
+              <div className={styles.plant}>
+                <div className={styles.plantInfo}>
+                  <Image
+                    className={styles.plantImage}
+                    src={romashiPhoto}
+                    alt={"personInitPage"}
+                    width={166}
+                    height={134}
+                  />
+                  <p className={styles.plantName}> Romashka </p>
+                </div>
+              </div>
+              <div className={styles.plant}>
+                <div className={styles.plantInfo}>
+                  <Image
+                    className={styles.plantImage}
+                    src={romashiPhoto}
+                    alt={"personInitPage"}
+                    width={166}
+                    height={134}
+                  />
+                  <p className={styles.plantName}> Romashka </p>
+                </div>
+              </div>
+              <div className={styles.plant}>
+                <div className={styles.plantInfo}>
+                  <Image
+                    className={styles.plantImage}
+                    src={romashiPhoto}
+                    alt={"personInitPage"}
+                    width={166}
+                    height={134}
+                  />
+                  <p className={styles.plantName}> Romashka </p>
+                </div>
+              </div>
+              <div className={styles.plant}>
+                <div className={styles.plantInfo}>
+                  <Image
+                    className={styles.plantImage}
+                    src={romashiPhoto}
+                    alt={"personInitPage"}
+                    width={166}
+                    height={134}
+                  />
+                  <p className={styles.plantName}> Romashka </p>
+                </div>
+              </div>
+              <div className={styles.plant}>
+                <div className={styles.plantInfo}>
+                  <Image
+                    className={styles.plantImage}
+                    src={romashiPhoto}
+                    alt={"personInitPage"}
+                    width={166}
+                    height={134}
+                  />
+                  <p className={styles.plantName}> Romashka </p>
+                </div>
+              </div>
+              <div className={styles.plant}>
+                <div className={styles.plantInfo}>
+                  <Image
+                    className={styles.plantImage}
+                    src={romashiPhoto}
+                    alt={"personInitPage"}
+                    width={166}
+                    height={134}
+                  />
+                  <p className={styles.plantName}> Romashka </p>
+                </div>
+              </div>
+          </div>
       </div>
     </div>
   );
