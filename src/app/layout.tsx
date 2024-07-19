ecbd0ea894432e2e738c8e52c3423d976224ac6e
satoru gojo <eternalblessed99@gmail.com>
2024-07-19 17:36:13 +0300

    feat: add menu for mobile devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,83 +1,68 @@
-"use client";
+"use client"
 
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useState } from "react";
 import Link from "next/link";
 import "./globals.css";
 import styles from "./styles/layout.module.css";
 
 type LayoutProps = {
-  children: ReactNode;
+    children: ReactNode;
+    currentPath: string;
 };
 
-const Layout = ({ children }: LayoutProps) => {
-  const [activeButton, setActiveButton] = useState<string>("");
+const Layout = ({ children, currentPath }: LayoutProps) => {
+    const [menuOpen, setMenuOpen] = useState(false);
 
-  useEffect(() => {
-    const currentPath = window.location.pathname;
-    setActiveButton(currentPath);
-  }, []);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
-  const handleButtonClick = (path: string) => {
-    setActiveButton(path);
-  };
-
-  return (
-    <html>
-      <head>
-        <title>plantCare</title>
-      </head>
-      <body>
+    return (
+        <html>
+        <head>
+            <title>plantCare</title>
+        </head>
+        <body>
         <header>
-          <nav>
-            <p id="title">PlantCARE</p>
-            <Link
-              href="/"
-              passHref
-              className={`${styles.button3} ${activeButton === "/" ? styles.activeButton : ""}`}
-              onClick={() => handleButtonClick("/")}
-            >
-              Home Page
-            </Link>
-            <Link
-              href="/about"
-              passHref
-              className={`${styles.button3} ${activeButton === "/about" ? styles.activeButton : ""}`}
-              onClick={() => handleButtonClick("/about")}
-            >
-              About
-            </Link>
-            <Link
-              href="/profile"
-              passHref
-              className={`${styles.button3} ${activeButton === "/profile" ? styles.activeButton : ""}`}
-              onClick={() => handleButtonClick("/profile")}
-            >
-              Profile
-            </Link>
-            <Link
-              href="/search"
-              passHref
-              className={`${styles.button3} ${activeButton === "/search" ? styles.activeButton : ""}`}
-              onClick={() => handleButtonClick("/search")}
-            >
-              Search
-            </Link>
-            <Link
-              href="/calendar"
-              passHref
-              className={`${styles.button3} ${activeButton === "/calendar" ? styles.activeButton : ""}`}
-              onClick={() => handleButtonClick("/calendar")}
-            >
-              Calendar
-            </Link>
-          </nav>
+            <nav className={styles.nav}>
+                <p id="title">PlantCARE</p>
+                <button className={styles.menuButton} onClick={toggleMenu}>
+                    {'☰'}
+                </button>
+                <div className={`${styles.navLinks} ${menuOpen ? styles.showMenu : ""}`}>
+                    <Link href="/" passHref>
+                        <button className={`${styles.button3} ${currentPath === "/" ? styles.activeButton : ""}`} onClick={toggleMenu}>
+                            Home Page
+                        </button>
+                    </Link>
+                    <Link href="/about" passHref>
+                        <button className={`${styles.button3} ${currentPath === "/about" ? styles.activeButton : ""}`} onClick={toggleMenu}>
+                            About
+                        </button>
+                    </Link>
+                    <Link href="/profile" passHref>
+                        <button className={`${styles.button3} ${currentPath === "/profile" ? styles.activeButton : ""}`} onClick={toggleMenu}>
+                            Profile
+                        </button>
+                    </Link>
+                    <Link href="/search" passHref>
+                        <button className={`${styles.button3} ${currentPath === "/search" ? styles.activeButton : ""}`} onClick={toggleMenu}>
+                            Search
+                        </button>
+                    </Link>
+                    <Link href="/calendar" passHref>
+                        <button className={`${styles.button3} ${currentPath === "/calendar" ? styles.activeButton : ""}`} onClick={toggleMenu}>
+                            Calendar
+                        </button>
+                    </Link>
+                </div>
+            </nav>
         </header>
         <main>{children}</main>
-
         <footer></footer>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 };
 
 export default Layout;
