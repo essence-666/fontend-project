27ab00bda33bd8c1039a4371607e7f342ad0064d
essence- <6ejlo3epobnt@gmail.com>
2024-07-20 02:34:42 +0300

    fix():

--- src/app/editFlowers/[id]/page.tsx
+++ src/app/editFlowers/[id]/page.tsx
@@ -12,18 +12,18 @@ import { useRouter } from "next/navigation";
 import { URL } from "@/app/config";
 
 const getFlowerById = async (id: string) => {
-    try {
-      const res = await fetch(`${URL}/api/flowersApi/${id}`, {
-        cache: "no-store",
-      });
-      if (!res.ok) {
-        throw new Error("Failed to get flower editions");
-      }
-      return res.json();
-    } catch (error) {
-      console.log(error);
+  try {
+    const res = await fetch(`${URL}/api/flowersApi/${id}`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to get flower editions");
     }
-  };
+    return res.json();
+  } catch (error) {
+    return [];
+  }
+};
 
 const EditFlowerPage = ({ params }: { params: { id: string } }) => {
   const { id } = params;
@@ -50,143 +50,148 @@ const EditFlowerPage = ({ params }: { params: { id: string } }) => {
     fetchProfile();
   }, [id]);
 
-    // Измените тип события на более конкретный
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  // Измените тип события на более конкретный
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        // if (!name || !location || !frequencyWatering || !wateringChanges) {
-        //     alert("All required fields are required");
-        //     return;
-        // }
+    // if (!name || !location || !frequencyWatering || !wateringChanges) {
+    //     alert("All required fields are required");
+    //     return;
+    // }
 
-        try {
-            const res = await fetch(`${URL}/api/flowersApi/${id}`, {
-                method: "PUT",
-                headers: {
-                    "Content-type": "application/json"
-                },
-                body: JSON.stringify({ newName, newScientificName, newLocation, newFrequencyWatering, newWateringChanges })
-            });
+    try {
+      const res = await fetch(`${URL}/api/flowersApi/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          newName,
+          newScientificName,
+          newLocation,
+          newFrequencyWatering,
+          newWateringChanges,
+        }),
+      });
 
-            if (res.ok) {
-                router.push("/profile");
-            } else {
-                throw new Error("Failed to create a flower");
-            }
+      if (res.ok) {
+        router.push("/profile");
+      } else {
+        throw new Error("Failed to create a flower");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className={styles.container}>
-                <p className={styles.path}>Home {">"} Edit flower</p>
-                <div className={styles.editContainer}>
-                    <div className={styles.nameAndPhoto}>
-                        <div className={styles.name}>
-                            <div className={styles.nameAndIcon}>
-                                <div className={styles.head}>Name</div>
-                                <Image
-                                    className={styles.image}
-                                    src={editPhoto}
-                                    alt={"Edit icon"}
-                                    width={23}
-                                    height={23}
-                                />
-                            </div>
-                            <input 
-                                onChange={(e) => setNewName(e.target.value)}
-                                value={newName}
-                                className={styles.labelContainer} 
-                                placeholder="Name..."
-                            />
-                        </div>
-                        <div className={styles.photo}>
-                            <div className={styles.head}>Photo</div>
-                            <div className={styles.placeForPhoto}>
-                                <Image
-                                    className={styles.image}
-                                    src={photoPhoto}
-                                    alt={"Photo icon"}
-                                    width={23}
-                                    height={23}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.scientificName}>
-                        <div className={styles.nameAndIconAndName}>
-                            <div className={styles.head}>Scientific name</div>
-                            <Image
-                                className={styles.image}
-                                src={lupaPhoto}
-                                alt={"Scientific name icon"}
-                                width={23}
-                                height={23}
-                            />
-                            <div className={styles.head}>(optional)</div>
-                        </div>
-                        <input 
-                            onChange={(e) => setScientificName(e.target.value)}
-                            value={newScientificName}
-                            className={styles.labelContainerLong} 
-                            placeholder="Scientific name..."
-                        />
-                    </div>
-                    <div className={styles.location}>
-                        <div className={styles.nameAndIconLong}>
-                            <div className={styles.head}>Location / Room</div>
-                            <Image
-                                className={styles.image}
-                                src={homePhoto}
-                                alt={"Location icon"}
-                                width={23}
-                                height={23}
-                            />
-                        </div>
-                        <input 
-                            onChange={(e) => setLocation(e.target.value)}
-                            value={newLocation}
-                            className={styles.labelContainerLong} 
-                            placeholder="Location..."
-                        />
-                    </div>
-                    <div className={styles.watering}>
-                        <div className={styles.nameAndIcon}>
-                            <div className={styles.head}>Watering</div>
-                            <Image 
-                                className={styles.image}
-                                src={dropPhoto}
-                                alt={"Watering icon"}
-                                width={23}
-                                height={23}
-                            />
-                        </div>
-                        <div className={styles.wateringContainer}>
-                            <div className={styles.head}>Every</div>
-                            <input 
-                                onChange={(e) => setFrequencyWatering(e.target.value)}
-                                value={newFrequencyWatering}
-                                className={styles.labelContainerSmall} 
-                                placeholder="Number..."
-                            />
-                            <input 
-                                onChange={(e) => setWateringChanges(e.target.value)}
-                                value={newWateringChanges}
-                                className={styles.labelContainerMiddle} 
-                                placeholder="Days/Weeks/Months..."
-                            />
-                            <div className={styles.buttonContainer}>
-                                <button className={styles.saveButton}>Save</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={styles.container}>
+        <p className={styles.path}>Home {">"} Edit flower</p>
+        <div className={styles.editContainer}>
+          <div className={styles.nameAndPhoto}>
+            <div className={styles.name}>
+              <div className={styles.nameAndIcon}>
+                <div className={styles.head}>Name</div>
+                <Image
+                  className={styles.image}
+                  src={editPhoto}
+                  alt={"Edit icon"}
+                  width={23}
+                  height={23}
+                />
+              </div>
+              <input
+                onChange={(e) => setNewName(e.target.value)}
+                value={newName}
+                className={styles.labelContainer}
+                placeholder="Name..."
+              />
             </div>
-        </form>
-    );
+            <div className={styles.photo}>
+              <div className={styles.head}>Photo</div>
+              <div className={styles.placeForPhoto}>
+                <Image
+                  className={styles.image}
+                  src={photoPhoto}
+                  alt={"Photo icon"}
+                  width={23}
+                  height={23}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.scientificName}>
+            <div className={styles.nameAndIconAndName}>
+              <div className={styles.head}>Scientific name</div>
+              <Image
+                className={styles.image}
+                src={lupaPhoto}
+                alt={"Scientific name icon"}
+                width={23}
+                height={23}
+              />
+              <div className={styles.head}>(optional)</div>
+            </div>
+            <input
+              onChange={(e) => setScientificName(e.target.value)}
+              value={newScientificName}
+              className={styles.labelContainerLong}
+              placeholder="Scientific name..."
+            />
+          </div>
+          <div className={styles.location}>
+            <div className={styles.nameAndIconLong}>
+              <div className={styles.head}>Location / Room</div>
+              <Image
+                className={styles.image}
+                src={homePhoto}
+                alt={"Location icon"}
+                width={23}
+                height={23}
+              />
+            </div>
+            <input
+              onChange={(e) => setLocation(e.target.value)}
+              value={newLocation}
+              className={styles.labelContainerLong}
+              placeholder="Location..."
+            />
+          </div>
+          <div className={styles.watering}>
+            <div className={styles.nameAndIcon}>
+              <div className={styles.head}>Watering</div>
+              <Image
+                className={styles.image}
+                src={dropPhoto}
+                alt={"Watering icon"}
+                width={23}
+                height={23}
+              />
+            </div>
+            <div className={styles.wateringContainer}>
+              <div className={styles.head}>Every</div>
+              <input
+                onChange={(e) => setFrequencyWatering(e.target.value)}
+                value={newFrequencyWatering}
+                className={styles.labelContainerSmall}
+                placeholder="Number..."
+              />
+              <input
+                onChange={(e) => setWateringChanges(e.target.value)}
+                value={newWateringChanges}
+                className={styles.labelContainerMiddle}
+                placeholder="Days/Weeks/Months..."
+              />
+              <div className={styles.buttonContainer}>
+                <button className={styles.saveButton}>Save</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default EditFlowerPage;
--- src/app/flowerElement/page.tsx
+++ src/app/flowerElement/page.tsx
@@ -1,86 +1,104 @@
 "use client";
 
-import { useSearchParams } from 'next/navigation';
-import Image from 'next/image';
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
+import Image from "next/image";
 import styles from "../styles/flowerElement.module.css";
 import romashkiPhoto from "../../../public/assets/romashki.png";
 import editPhoto from "../../../public/assets/editSecond.png";
 import delPhoto from "../../../public/assets/trashCan.png";
-import Link from 'next/link';
+import Link from "next/link";
 import { URL } from "../config";
 
-
-
 const FlowerElement = () => {
-    const searchParams = useSearchParams();
-    const id = searchParams.get('_id');
-    const name = searchParams.get('name');
-    const scientificName = searchParams.get('scientificName');
-    const location = searchParams.get('location');
-    const frequencyWatering = searchParams.get('frequencyWatering');
-    const wateringChanges = searchParams.get('wateringChanges');
+  const searchParams = useSearchParams();
+  if (!searchParams) return <div>Loading...</div>;
 
-    const removeFlower = async () => {
-        const confirmed = confirm("Are you sure?");
-    
-        if (confirmed) {
-            await fetch(`${URL}/api/flowersApi?id=${id}`, {
-                method: "DELETE",
-            });
+  const id = searchParams.get("_id");
+  const name = searchParams.get("name");
+  const scientificName = searchParams.get("scientificName");
+  const location = searchParams.get("location");
+  const frequencyWatering = searchParams.get("frequencyWatering");
+  const wateringChanges = searchParams.get("wateringChanges");
+
+  const removeFlower = async () => {
+    const confirmed = confirm("Are you sure?");
+
+    if (confirmed) {
+      try {
+        const res = await fetch(`${URL}/api/flowersApi?id=${id}`, {
+          method: "DELETE",
+        });
+        if (!res.ok) {
+          throw new Error("Failed to delete flower");
         }
+        alert("Flower deleted successfully");
+      } catch (error) {
+        console.error("Error deleting flower:", error);
+        alert("An error occurred while deleting the flower");
+      }
     }
+  };
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.containerFlower}>
-                <Image 
-                    className={styles.image}
-                    src={romashkiPhoto}
-                    alt={"flowerImage"}
-                    width={250}
-                    height={250}
-                />               
-                <div className={styles.textContainer}>
-                    <div className={styles.infoElements}>
-                        <div className={styles.infoTitle}>Name:</div>
-                        <div className={styles.infocontent}>{name}</div>
-                    </div>
-                    <div className={styles.infoElements}>
-                        <div className={styles.infoTitle}>Scientific name:</div>
-                        <div className={styles.infocontent}>{scientificName}</div>
-                    </div>
-                    <div className={styles.infoElements}>
-                        <div className={styles.infoTitle}>Location:</div>
-                        <div className={styles.infocontent}>{location}</div>
-                    </div>
-                    <div className={styles.infoElements}>
-                        <div className={styles.infoTitle}>Watering frequency:</div>
-                        <div className={styles.infocontent}>{frequencyWatering}  {wateringChanges}</div>
-                    </div>
-                </div>
-                <div className={styles.buttonContainer}>
-                    <Link className={styles.button} href={`/editFlowers/${id}`}>
-                        <Image 
-                            className={styles.buttonImage}
-                            src={editPhoto}
-                            alt={"editBtnImg"}
-                            width={50}
-                            height={50}
-                        />
-                    </Link>
-                    <Link onClick={removeFlower} className={styles.button} href={"/profile"}>
-                        <Image 
-                            className={styles.buttonImage}
-                            src={delPhoto}
-                            alt={"delBtnImg"}
-                            width={50}
-                            height={50}
-                        />
-                    </Link>
-                </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerFlower}>
+        <Image
+          className={styles.image}
+          src={romashkiPhoto}
+          alt={"flowerImage"}
+          width={250}
+          height={250}
+        />
+        <div className={styles.textContainer}>
+          <div className={styles.infoElements}>
+            <div className={styles.infoTitle}>Name:</div>
+            <div className={styles.infocontent}>{name}</div>
+          </div>
+          <div className={styles.infoElements}>
+            <div className={styles.infoTitle}>Scientific name:</div>
+            <div className={styles.infocontent}>{scientificName}</div>
+          </div>
+          <div className={styles.infoElements}>
+            <div className={styles.infoTitle}>Location:</div>
+            <div className={styles.infocontent}>{location}</div>
+          </div>
+          <div className={styles.infoElements}>
+            <div className={styles.infoTitle}>Watering frequency:</div>
+            <div className={styles.infocontent}>
+              {frequencyWatering} {wateringChanges}
             </div>
+          </div>
         </div>
-    );
+        <div className={styles.buttonContainer}>
+          <Link className={styles.button} href={`/editFlowers/${id}`}>
+            <Image
+              className={styles.buttonImage}
+              src={editPhoto}
+              alt={"editBtnImg"}
+              width={50}
+              height={50}
+            />
+          </Link>
+          <button onClick={removeFlower} className={styles.button}>
+            <Image
+              className={styles.buttonImage}
+              src={delPhoto}
+              alt={"delBtnImg"}
+              width={50}
+              height={50}
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default FlowerElement;
+const FlowerElementPage = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <FlowerElement />
+  </Suspense>
+);
+
+export default FlowerElementPage;
