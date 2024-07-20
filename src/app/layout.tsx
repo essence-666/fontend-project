cc23b6500970f8e778e6ddbc95e57d63c8b580ac
satoru gojo <eternalblessed99@gmail.com>
2024-07-20 13:26:47 +0300

    refactor: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
 // src/app/layout.tsx
-"use client";
-
 import React, { ReactNode } from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import type {Metadata} from "next";
 import "./globals.css";
 import "@uploadthing/react/styles.css";
 import Header from "./components/Header"
 type LayoutProps = {
   children: ReactNode;
 };
+export const metadata: Metadata = {
+    title: 'Take care of your plants',
+    description: 'Do not forget to water your favorite flowers!',
+    keywords: "plants flowers watering health green dandelion orchid beauty",
+
+}
+
 
 const Layout = ({ children }: LayoutProps) => {
 
