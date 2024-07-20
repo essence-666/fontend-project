174c2d99433d3a4cda802eea31fdbed4e76c2988
essence- <6ejlo3epobnt@gmail.com>
2024-07-20 14:10:40 +0300

    fix(eslint): temporary disable eslint in building config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                hostname: "utfs.io"
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'utfs.io',
+      },
+    ],
+  },
+
+  eslint: {
+    /** change it to false for enable eslint */
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
